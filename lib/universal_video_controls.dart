07c27902cc510e3c7cdbd26d1766849e7e7a67bd
Abdelaziz Mahdy <zezohassam@gmail.com>
2024-05-30 19:25:51 +0300

    first commit

---
+++ lib/universal_video_controls.dart
@@ -0,0 +1,22 @@
+/// This file is a part of media_kit (https://github.com/media-kit/media-kit).
+///
+/// Copyright © 2021 & onwards, Hitesh Kumar Saini <saini123hitesh@gmail.com>.
+/// All rights reserved.
+/// Use of this source code is governed by MIT license that can be found in the LICENSE file.
+
+
+export 'package:universal_video_controls/src/video_view_parameters.dart';
+export 'package:universal_video_controls/src/video/video.dart';
+export 'package:universal_video_controls/src/subtitle/subtitle_view.dart';
+
+
+export 'package:universal_video_controls/universal_video_controls/src/controls/no.dart';
+export 'package:universal_video_controls/universal_video_controls/src/controls/adaptive.dart';
+
+export 'package:universal_video_controls/universal_video_controls/src/controls/material.dart';
+export 'package:universal_video_controls/universal_video_controls/src/controls/cupertino.dart';
+export 'package:universal_video_controls/universal_video_controls/src/controls/material_desktop.dart';
+export 'package:universal_video_controls/universal_video_controls/src/controls/methods/fullscreen.dart';
+export 'package:universal_video_controls/universal_video_controls/src/controls/widgets/fullscreen_inherited_widget.dart';
+export 'package:universal_video_controls/universal_video_controls/src/controls/widgets/video_state_inherited_widget.dart';
+
