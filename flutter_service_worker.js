752bdb7783d93a4653c6f617e02a78dacbfe3d82
abdelaziz-mahdy <abdelaziz-mahdy@users.noreply.github.com>
2024-08-08 21:14:50 +0000

    deploy: 1e587ec5245ff31566f9654d4f8f474216f00ae4

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -25,7 +25,7 @@ const RESOURCES = {"icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb0489
 "main.dart.js": "c3515ed6f464cab9dff48536b9417164",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "version.json": "b976f974da29d4d83ff00709267fb5fb",
-"flutter_bootstrap.js": "75686bec229c81d1d130f2423786f593",
+"flutter_bootstrap.js": "e4e6307f250d6586f40080196c03a5de",
 "canvaskit/skwasm.js.symbols": "262f4827a1317abb59d71d6c587a93e2",
 "canvaskit/skwasm.wasm": "9f0c0c02b82a910d12ce0543ec130e60",
 "canvaskit/chromium/canvaskit.wasm": "b1ac05b29c127d86df4bcfbf50dd902a",
