9d512ce4435490bf07ebdc7fb5c9e439fdedf4b2
abdelaziz-mahdy <abdelaziz-mahdy@users.noreply.github.com>
2024-08-12 21:38:09 +0000

    deploy: 3c6c66ff88050194045f597b62723c021412a2b5

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "608397898"
+    serviceWorkerVersion: "3753831855"
   }
 });
