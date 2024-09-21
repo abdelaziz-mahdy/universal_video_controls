c5887a2c8a14e6ca4495d06f906b96c6d044b3fd
abdelaziz-mahdy <abdelaziz-mahdy@users.noreply.github.com>
2024-09-21 15:45:29 +0000

    deploy: 9c32e122f2972ff1bf29a1666b48950891bdab2b

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -27,7 +27,7 @@ const RESOURCES = {"assets/AssetManifest.json": "0d8be223df64f684a30d1c7caf8e548
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
-"flutter_bootstrap.js": "fe22c1d33ac701817c0169ecd0c9c4d8",
+"flutter_bootstrap.js": "2293b6cbfb5ad5f542e5057c6332c701",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "index.html": "4791b0cf0a1de19540e8cf9ecb8cc889",
