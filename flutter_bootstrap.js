097bda0b60a50d9e2e4bbd0fc1f93f2aeaa76e8a
abdelaziz-mahdy <abdelaziz-mahdy@users.noreply.github.com>
2024-09-21 15:52:28 +0000

    deploy: f2aebd2043ac6473b13e1d40e88297310c39ac7e

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3047960830"
+    serviceWorkerVersion: "2459043466"
   }
 });
--- wasm/flutter_service_worker.js
+++ wasm/flutter_service_worker.js
@@ -27,7 +27,7 @@ const RESOURCES = {"assets/AssetManifest.json": "0d8be223df64f684a30d1c7caf8e548
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
-"flutter_bootstrap.js": "b68d625f662dfdf3db180dd1c9ba5f31",
+"flutter_bootstrap.js": "ac82901d967ceb3885ef04c0804483d3",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "index.html": "33072a39476b7ba91acab2e43aae4e49",
