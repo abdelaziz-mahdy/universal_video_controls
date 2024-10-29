4eed9fb7ed70c360cf2553dd718413c4d374e149
abdelaziz-mahdy <abdelaziz-mahdy@users.noreply.github.com>
2024-10-29 16:26:42 +0000

    deploy: 892f479430aeb01957dc6d3914301c60c7b95503

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -33,7 +33,7 @@ const RESOURCES = {"flutter.js": "f393d3c16b631f36852323de8e583132",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "version.json": "b976f974da29d4d83ff00709267fb5fb",
-"flutter_bootstrap.js": "174ab5b049df94520681c26b7f37c148"};
+"flutter_bootstrap.js": "155b9b4fc00061a63922aef3f6d19c33"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
--- wasm/flutter_service_worker.js
+++ wasm/flutter_service_worker.js
@@ -35,7 +35,7 @@ const RESOURCES = {"flutter.js": "f393d3c16b631f36852323de8e583132",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "version.json": "b976f974da29d4d83ff00709267fb5fb",
-"flutter_bootstrap.js": "4f2ca7933fafb0a0022c97e0c795c073"};
+"flutter_bootstrap.js": "82c0cf82ec64be43994852d6195aa0f8"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
